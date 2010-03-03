b19cc016395f6c99a48b15d3a82d7624d20246aa
Lukas Fryc <lfryc@redhat.com>
2010-03-03 11:09:43 +0000

    modifications of templates and run_all.sh to work properly

--- run_all.sh
+++ run_all.sh
@@ -1,2 +1,2 @@
-WORK_DIR=test_data_
-java -cp bin cz.tisnik.ImageDiff.Main -s1=${WORK_DIR}/source1 -s2=${WORK_DIR}/source2 -masks=${WORK_DIR}/masks -diffs=${WORK_DIR}/diffs -htmlout=${WORK_DIR}/html
+WORK_DIR=screenshots
+java -cp target/classes cz.tisnik.ImageDiff.Main -s1=${WORK_DIR}/screenshots-3.3.3.BETA1 -s2=${WORK_DIR}/screenshots-3.3.3.CR1 -masks=${WORK_DIR}/masks -diffs=${WORK_DIR}/diffs -htmlout=${WORK_DIR}/diffs -structdiffs=${WORK_DIR}/diffs
