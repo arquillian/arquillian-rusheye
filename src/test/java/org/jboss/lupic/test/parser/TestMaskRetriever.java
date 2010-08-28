3d79706cfe41e28f09e8d12cbe9591a87e41eece
Lukas Fryc <lfryc@redhat.com>
2010-08-28 20:44:06 +0000

    added test for mask-retriever and fixed visual-suite.xsd for complex content of mask-retriever element

---
+++ src/test/java/org/jboss/lupic/test/parser/TestMaskRetriever.java
@@ -0,0 +1,32 @@
+package org.jboss.lupic.test.parser;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.jboss.lupic.retriever.Retriever;
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+import static org.testng.Assert.*;
+
+public class TestMaskRetriever extends AbstractVisualSuiteDefinitionTest {
+
+	private final static String SOURCE = "source";
+
+	@Test
+	public void testPropertiesShouldPass() throws SAXException, IOException {
+		String retrieverImpl = TestImageRetriever.AssertingRetriever.class
+				.getName();
+		stub.maskRetriever.addAttribute("class", retrieverImpl);
+
+		stub.maskRetriever.addElement("xxx").setText("1");
+		stub.maskRetriever.addElement("yyy").setText("2");
+
+		startWriter();
+		parse();
+
+		Retriever retriever = handler.getVisualSuite().getGlobalConfiguration()
+				.getMaskRetriever();
+		assertNull(retriever.retrieve(SOURCE, new Properties()));
+	}
+}
