96abe53b690bef911df4bc7a302ba0aefe96afd4
Juraj Huska <gooseka2@gmail.com>
2014-12-03 00:30:20 +0100

    ParsingDoneEvent fired after parsing is done

--- rusheye-impl/src/main/java/org/jboss/rusheye/arquillian/observer/ParseObserver.java
+++ rusheye-impl/src/main/java/org/jboss/rusheye/arquillian/observer/ParseObserver.java
@@ -2,10 +2,12 @@ package org.jboss.rusheye.arquillian.observer;
 
 import com.beust.jcommander.IStringConverter;
 import java.io.File;
+import org.jboss.arquillian.core.api.Event;
 import org.jboss.arquillian.core.api.Instance;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.core.api.annotation.Observes;
 import org.jboss.rusheye.arquillian.configuration.RusheyeConfiguration;
+import org.jboss.rusheye.arquillian.event.ParsingDoneEvent;
 import org.jboss.rusheye.arquillian.event.StartParsingEvent;
 import org.jboss.rusheye.internal.Instantiator;
 import org.jboss.rusheye.listener.SuiteListener;
@@ -21,24 +23,28 @@ public class ParseObserver {
     @Inject
     private Instance<RusheyeConfiguration> rusheyeConfiguration;
 
+    @Inject
+    private Event<ParsingDoneEvent> parsingDoneEvent;
+
     private Properties properties = new Properties();
     private final SuiteListenerConverter suiteListenerConverter = new SuiteListenerConverter();
 
     public void parse(@Observes StartParsingEvent event) {
         initialize(event);
-        
+
         Parser parser = new Parser();
         parser.setProperties(properties);
-        
+
         if (rusheyeConfiguration.get().getSuiteListener() != null) {
             SuiteListener converted = suiteListenerConverter.convert(rusheyeConfiguration.get().getSuiteListener());
             parser.registerListener(converted);
         }
-        
-        File suiteDescriptor = new File(event.getPatternAndDescriptorFolder() + 
-                File.separator + 
+
+        File suiteDescriptor = new File(event.getPatternAndDescriptorFolder() +
+                File.separator +
                 rusheyeConfiguration.get().getSuiteDescriptor());
         parser.parseFile(suiteDescriptor);
+        parsingDoneEvent.fire(new ParsingDoneEvent());
     }
 
     public void initialize(StartParsingEvent event) {
@@ -50,7 +56,7 @@ public class ParseObserver {
         properties.setProperty("file-storage-directory", conf.getWorkingDirectory() + File.separator + conf.getDiffsDir());
         properties.setProperty("masks-directory", conf.getMaskBase());
     }
-    
+
     private String getRelativePatternsDir(String samplesDir) {
         String absoluteWorking = rusheyeConfiguration.get().getWorkingDirectory().getAbsolutePath();
         return samplesDir.replace(absoluteWorking, "");
